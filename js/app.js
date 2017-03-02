c3223992bdda78e523fae538a6f3093ce710a2d7
Karl Olofsson <karl.olofsson@solidsquare.no>
2017-03-02 13:43:48 +0100

    Activated rendering of game board. Also added 3 enemies and 1 player, as well as basic player movements and edge constraints.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 1;
+    this.y = y*83-190;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +15,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +27,56 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 3*67;
+    this.y = 6*65;
+};
 
+player.prototype.handleInput = function(key){
+    switch(key){
+        case 'left':
+            if(this.x>1)
+                this.x -= 100;
+            break;
+        case 'up':
+            if(this.y>1){
+                if(this.y<120){
+                    player.reset();
+                    break;
+                }
+                this.y -= 85;
+            }
+            break;
+        case 'right':
+            if(this.x<5*67)
+                this.x += 100;
+            break;
+        case 'down':
+            if(this.y<6*65)
+                this.y += 85;
+            break;
+        default:
+            break;
+    };
+    player.update();
+};
+player.prototype.reset = function(){
+    this.x = 1*5*40;
+    this.y = 1*6*65;
+}
+player.prototype.update = function(){
+    player.render();
+};
+player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [new Enemy(3),new Enemy(4),new Enemy(5)];
+var player = new player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
